aeb7e5e71f96bb4ef97b1a84f1d82ac24709c843
Kyuwoong Lee <kwooler@gmail.com>
2023-05-24 18:19:34 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+    i = 0
+    longest = shortest = ''
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return longest, shortest
+##
+
+
+if __name__ == '__main__':
+    main()
